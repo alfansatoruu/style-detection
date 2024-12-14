cbdf2679bab0b03639796b8851503f9260b31d29
alfansatoruu <hellb2254@gmail.com>
2024-12-14 13:22:05 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compression()],
   build: {
-    outDir: 'dist', // Direktori output untuk file hasil build
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
   },
-  base: '/style/', // Penting untuk subpath di GitHub Pages
+  base: '/style-detection/',
 });
-
